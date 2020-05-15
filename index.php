4a50b3a91dd07dc3e366f313223fe4593e4152e8
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-15 15:10:11 +0200

    completed

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <?php
         $text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem deserunt beatae tempore praesentium facere, accusantium fugiat odit ullam ipsum iste iure, ducimus et quo itaque voluptatibus reiciendis! Quos, illum at.';
 
-
+        $search_word = $_GET["search"];
     ?>    
 
     <main>
@@ -17,7 +17,19 @@
         <p> 
             <?php echo $text; ?>
         </p>
-    </main>
 
+        <p>
+            The lenght is: <strong><?php echo strlen($text);?></strong>.
+        </p>
+
+        <p>
+            The search word is "<strong><?php echo $search_word;?></strong>"".
+        </p>
+
+        <p>
+            Now the each occurance of "search_word" is replaced by "***":<br><br>
+            <?php echo str_replace($search_word, '***', $text); ?>
+        </p>
+    </main>
 </body>
 </html>
