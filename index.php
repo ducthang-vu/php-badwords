2327579b59f7eafa42de700ad85064f60359c257
Duc Thang Vu <vuducthang91@gmail.com>
2020-05-15 14:55:54 +0200

    first two tasks completed

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $text = 'Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem deserunt beatae tempore praesentium facere, accusantium fugiat odit ullam ipsum iste iure, ducimus et quo itaque voluptatibus reiciendis! Quos, illum at.';
+
+
+    ?>    
+
+    <main>
+        <h1>The text is:</h1>
+        <p> 
+            <?php echo $text; ?>
+        </p>
+    </main>
+
+</body>
+</html>
